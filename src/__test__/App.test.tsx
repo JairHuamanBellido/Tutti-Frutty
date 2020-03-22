a0e7fd3d3fb8b00bdb518b04bd986de478f98dd0
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2020-03-22 18:57:26 -0500

    Fix test

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -4,7 +4,7 @@ import App from "../App";
 
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import RegisterPage from "../Pages/Register/Register";
+
 
 configure({ adapter: new Adapter() });
 
