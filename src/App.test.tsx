ff2206393a98718d2efe03f91e85376cd4beed4e
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2020-03-22 11:38:15 -0500

    Project init

---
+++ src/App.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { shallow } from 'enzyme';
+import App from "./App";
+
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+describe('link', () => {
+  it('Renders link to Google', () => {
+    const app = shallow(<App />);
+    expect(app.contains(<h1>Hola mundo</h1>)).toBe(true);
+  });
+})
